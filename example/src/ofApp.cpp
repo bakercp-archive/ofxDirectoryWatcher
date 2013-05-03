3777d94454c57b9f14cd896ae7fe24baca0191f6
Christopher Baker <me@christopherbaker.net>
2013-05-02 23:23:26 -0500

    Updated example to match core updates.

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,77 +1,47 @@
 #include "ofApp.h"
 
 void ofApp::exit() {
-    ofRemoveListener(watcher.events.onItemAdded,    this, &ofApp::onItemAdded);
-    ofRemoveListener(watcher.events.onItemModified, this, &ofApp::onItemModified);
-    ofRemoveListener(watcher.events.onItemRemoved,  this, &ofApp::onItemRemoved);
-    ofRemoveListener(watcher.events.onItemMovedTo,  this, &ofApp::onItemMovedTo);
-    ofRemoveListener(watcher.events.onItemRemoved,  this, &ofApp::onItemRemoved);
+    watcher.UnregisterBrowseServiceEvents(this);
 }
 
 //------------------------------------------------------------------------------
 void ofApp::setup(){
-    
-    watcher.watch(ofToDataPath(""));
-    
-    ofAddListener(watcher.events.onItemAdded,    this, &ofApp::onItemAdded);
-    ofAddListener(watcher.events.onItemModified, this, &ofApp::onItemModified);
-    ofAddListener(watcher.events.onItemRemoved,  this, &ofApp::onItemRemoved);
-    ofAddListener(watcher.events.onItemMovedTo,  this, &ofApp::onItemMovedTo);
-    ofAddListener(watcher.events.onItemRemoved,  this, &ofApp::onItemRemoved);
-    
-}
 
-//------------------------------------------------------------------------------
-void ofApp::update(){
+    watcher.RegisterBrowseServiceEvents(this);
+
+    ofDirectory dir("");
+    ofFile file("");
+
+    watcher.setup(ofToDataPath("",true),true);
 
 }
 
 //------------------------------------------------------------------------------
 void ofApp::draw(){
+    ofBackground(0);
+    ofFill();
 
-}
+    int y = TXT_HEIGHT;
+    for(size_t i = 0; i < messages.size(); i++) {
 
-//------------------------------------------------------------------------------
-void ofApp::keyPressed(int key){
+        ofSetColor(ofMap(i, 0, messages.size(), 255, 90));
 
-}
-
-//------------------------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//------------------------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//------------------------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//------------------------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//------------------------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//------------------------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
+        ofDrawBitmapString(messages[i], 10, y);
+        y += TXT_HEIGHT;
+    }
 }
 
 //------------------------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
 
-}
+    int height = ofGetHeight();
+    int numLines = (int)(height / TXT_HEIGHT);
 
-//------------------------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
+    messages.push_front(msg.message);
+
+    while(messages.size() > numLines) {
+        messages.pop_back();
+    }
+
 
 }
--- example/src/ofApp.h
+++ example/src/ofApp.h
@@ -1,50 +1,48 @@
 #pragma once
 
+#include <deque>
 #include "ofMain.h"
-
 #include "ofxDirectoryWatcher.h"
 
-class ofApp : public ofBaseApp {
 
+class ofApp : public ofBaseApp {
 public:
+    enum {
+        TXT_HEIGHT = 14
+    };
+
     void exit();
     void setup();
-    void update();
     void draw();
 
-    void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
 
-    void onItemAdded(const ofxDirectoryEvent& evt) {
-        ofLogNotice("ofApp::onItemAdded") << "Added: " << evt.item.getAbsolutePath();
+    void onDirectoryItemAdded(const ofxDirectoryItemEvent& evt) {
+        ofSendMessage("Added: " + evt.item.getAbsolutePath());
     }
     
-    void onItemRemoved(const ofxDirectoryEvent& evt) {
-        ofLogNotice("ofApp::onItemRemoved") << "Removed: " << evt.item.getAbsolutePath();
+    void onDirectoryItemRemoved(const ofxDirectoryItemEvent& evt) {
+        ofSendMessage("Removed: " + evt.item.getAbsolutePath());
     }
     
-    void onItemModified(const ofxDirectoryEvent& evt) {
-        ofLogNotice("ofApp::onItemModified") << "Modified: " << evt.item.getAbsolutePath();
+    void onDirectoryItemModified(const ofxDirectoryItemEvent& evt) {
+        ofSendMessage("Modified: " + evt.item.getAbsolutePath());
     }
     
-    void onItemMovedFrom(const ofxDirectoryEvent& evt) {
+    void onDirectoryItemMovedFrom(const ofxDirectoryItemMoveEvent& evt) {
         ofLogNotice("ofApp::onItemMovedFrom") << "Moved From: " << evt.item.getAbsolutePath();
     }
     
-    void onItemMovedTo(const ofxDirectoryEvent& evt) {
+    void onDirectoryItemMovedTo(const ofxDirectoryItemMoveEvent& evt) {
         ofLogNotice("ofApp::onItemMovedTo") << "Moved To: " << evt.item.getAbsolutePath();
     }
     
-    void onScanError(const Exception& exc) {
+    void onDirectoryScanError(const Exception& exc) {
         ofLogError("ofApp::onScanError") << "Error: " << exc.displayText();
     }
     
     ofxDirectoryWatcher watcher;
+
+    std::deque<string> messages;
+
 };
